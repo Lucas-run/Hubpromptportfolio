6e71c7ed82212f96140f04d6e0505c5285305c7d
Lucas Ramos <dev.lucas.contato@gmail.com>
2025-10-21 18:31:00 -0300

    centralizando itens

--- src/components/AreaCard.tsx
+++ src/components/AreaCard.tsx
@@ -28,7 +28,6 @@ export const AreaCard = ({
         type="button"
       >
         <span className={styles.areaTitle}>{area.title}</span>
-        <span className={styles.chev}>{expanded ? "▾" : "▸"}</span>
       </button>
 
       <div
